b827692fff549f2be60b2a6c045777cae22cbf11
KurraHarika <121171553+KurraHarika@users.noreply.github.com>
2022-12-26 16:44:27 +0530

    Add files via upload

---
+++ day5program8.py
@@ -0,0 +1,10 @@
+s1='"Hi mom", I said. "How are you?"'
+print(s1)
+s2='"Hi mom", I said. '"How are you?"
+print(s2)
+s3='"Hi mom", I said. ''"How are you?"'
+print(s3
+s4="""'Hi mom", I said. '"How are you?"'"""
+print(s4
+s5=""I want to be a lion tamer!"'
+print(s5)
